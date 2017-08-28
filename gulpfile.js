2ef4fdb10e4e095a05617cedf8daacc088f4341c
Olu Meduoye <olu@claymoreminds.com>
2017-08-28 13:40:29 +0100

    adding tiles

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// Include gulp
+var gulp = require('gulp');
+// Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task( 'default', [ 'serve' ] );
+
+// Concatenate JS Files
+gulp.task('scripts', function() {
+    return gulp.src(['js/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ sourceMap: 'sass',outputStyle: 'nested'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function () {
+    // Watch .js files
+    gulp.watch(['js/*.js'], ['scripts']);
+    // Watch .scss files
+    gulp.watch('sass/*.scss', ['sass']);
+});
